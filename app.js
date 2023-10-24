8b1163367e373859456f352d24180f0781a3197e
RostaJecna <levder@spsejecna.cz>
2023-10-24 17:39:46 +0200

    Render `.ejs` file.

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (_req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 app.listen(port, () => {
