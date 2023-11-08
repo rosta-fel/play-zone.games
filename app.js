51259da35f52c18acbad6056f5ad1c07b36f8b31
RostaJecna <levder@spsejecna.cz>
2023-11-08 10:21:14 +0100

    Add render engine.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-    res.render('index');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (_req, res) => {
+  res.render("index");
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
