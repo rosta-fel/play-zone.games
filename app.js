92ea936dcda070a34e7340a5ed6d636dc691c8e4
r0usta <levder@spsejecna.cz>
2023-12-27 20:24:22 +0100

    Add paths for `login` and `signup` pages.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (_req, res) => {
-  res.render("index");
-});
+app.get("/", (_req, res) => res.render("index"));
+app.get("/login", (_req, res) => res.render("login"));
+app.get("/signup", (_req, res) => res.render("signup"));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
