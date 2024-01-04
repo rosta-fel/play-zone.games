11b9cf4788f5995777deb1b3e9cb7720827cc5f4
r0usta <levder@spsejecna.cz>
2024-01-04 10:11:16 +0100

    Get port from `.env` and create session instance.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import session from "express-session";
 import passport from "passport";
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
 app.use(passport.initialize());
 
 app.get("/", (_req, res) => res.render("index", {currentPage: "index"}));
