5614d40693c13a5ed2c3c7c95056083708e25b21
r0usta <levder@spsejecna.cz>
2024-01-15 20:11:33 +0100

    Import added packages.

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 import express from "express";
-import session from "express-session";
+import session from "express-session"
 import passport from "passport";
+import cookieParser from 'cookie-parser';
+import bodyParser from "body-parser";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import methodOverride from "method-override";
 import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.APP_PORT;
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
 }))
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(passport.initialize());
+app.use(passport.session({}));
+app.use(methodOverride('_method'))
 
 // S-T-A-R-T: EXTERNAL FUNCTIONS
 
