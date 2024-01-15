30e134d0a7e30457d52521ca96263838b6e5fd15
r0usta <levder@spsejecna.cz>
2024-01-15 08:58:39 +0100

    Re-Structure `app` file, add `404` error redirect.

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ app.use(session({
 }))
 app.use(passport.initialize());
 
-app.get("/", (_req, res) => res.render("index", {currentPage: "index"}));
-app.get("/login", (_req, res) => res.render("login", {currentPage: "login"}));
-app.get("/signup", (_req, res) => res.render("signup", {currentPage: "signup"}));
+// S-T-A-R-T: EXTERNAL FUNCTIONS
+
+const renderPage = (res, pageName, additionalParams = {}) => {
+  const defaultParams = { currentPage: pageName };
+  const mergedParams = { ...defaultParams, ...additionalParams };
+  res.render(pageName, mergedParams);
+};
 
 function checkAuth(req, res, next) {
   if (req.isAuthenticated()) {
@@ -27,11 +31,18 @@ function checkAuth(req, res, next) {
   res.redirect('/login');
 }
 
-app.get("/chat", checkAuth, (_req, res) => res.render("chat", {currentPage: "chat"}));
+// E-N-D: EXTERNAL FUNCTIONS
 
+app.get("/", (_req, res) => renderPage(res, "index"));
+app.get("/login", (_req, res) => renderPage(res, "login"));
+app.get("/signup", (_req, res) => renderPage(res, "signup"));
+app.get("/chat", checkAuth, (_req, res) => renderPage(res, "chat"));
+app.get("/about-us", (_req, res) => renderPage(res, "about-us"));
 
-app.get("/about-us", (_req, res) => res.render("about-us", {currentPage: "about-us"}));
-
+app.get('*', (_req, res) => {
+  res.status(404);
+  renderPage(res, "utils/404");
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
