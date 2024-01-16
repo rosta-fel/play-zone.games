85c642842a88f4d8349162596fcc143b4e63a455
r0usta <levder@spsejecna.cz>
2024-01-16 18:11:59 +0100

    Add listener for users logouts.

--- public/js/header.js
+++ public/js/header.js
@@ -1,7 +1,13 @@
 const navbarItems = document.getElementById("navbar-items");
 const navbarToggle = document.getElementById("navbar-toggle");
+const formLogout = document.getElementById('logout-form')
+const logoutLink = document.getElementById('logout-link');
 
 navbarToggle.addEventListener("click", function () {
     navbarToggle.classList.toggle("on");
     navbarItems.classList.toggle("hidden")
+});
+
+logoutLink.addEventListener("click", function () {
+   formLogout.submit();
 });
