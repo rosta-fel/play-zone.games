082c180a3ac4ac9571319709e1bbcec739ed541d
r0usta <levder@spsejecna.cz>
2024-01-16 18:12:24 +0100

    Add new tooltip for logout description.

--- public/js/tooltips.js
+++ public/js/tooltips.js
@@ -5,3 +5,11 @@ tippy('#store-link', {
     inertia: true,
     theme: 'azure',
 });
+
+tippy('#logout-link', {
+    content: 'Click to logout!',
+    placement: 'bottom',
+    animation: 'shift-toward-extreme',
+    inertia: true,
+    theme: 'azure',
+})
