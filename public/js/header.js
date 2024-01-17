ae2d9938543109fd41a347310bbdcd82fa11bd6b
r0usta <levder@spsejecna.cz>
2024-01-17 08:50:01 +0100

    Adding event listeners for header.

--- public/js/header.js
+++ public/js/header.js
@@ -1,13 +1,15 @@
 const navbarItems = document.getElementById("navbar-items");
 const navbarToggle = document.getElementById("navbar-toggle");
-const formLogout = document.getElementById('logout-form')
-const logoutLink = document.getElementById('logout-link');
+const formLogout = document.getElementsByClassName('logout-form')
+const logoutLink = document.getElementsByClassName('logout-link');
 
 navbarToggle.addEventListener("click", function () {
     navbarToggle.classList.toggle("on");
     navbarItems.classList.toggle("hidden")
 });
 
-logoutLink.addEventListener("click", function () {
-   formLogout.submit();
-});
+for (let i = 0; i < logoutLink.length; i++) {
+    logoutLink[i].addEventListener("click", function () {
+        formLogout[i].submit();
+    });
+}
